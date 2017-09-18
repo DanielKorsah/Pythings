265727ddf91e5ebbd56dcad3e2bf83080a908129
Dan's Laptop <dkorsah15@aol.co.uk>
2017-09-18 20:01:26 +0100

    Completed Euler Problem 2

--- Euler_Problems/E2.py
+++ Euler_Problems/E2.py
@@ -1,7 +1,18 @@
 def fibonacci(n):
-    if n <= 1 :
+    if n <= 1:
         return n
     else:
-        return (n-1) + (n-2)
-for i in range (int.maxvalue):
-    
+        f = (fibonacci(n-1) + fibonacci(n-2))
+        return f
+
+
+t = 0
+for i in range(0, 100000):
+    if fibonacci(i) < 40000000 and fibonacci(i) % 2 == 0:
+        fib = fibonacci(i)
+        print(i)
+        print(fib)
+        t += fibonacci(i)
+    if fibonacci(i) > 4000000:
+        break
+print("answer = ", t)
